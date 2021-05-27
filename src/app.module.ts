6493d5d8265fd8a767a171c40d2c73125ed19443
Claudia <claudia@Claudias-MacBook-Pro.local>
2021-05-27 18:43:47 -0400

    add structure for workouts module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { WorkoutsModule } from './workouts/workouts.module';
 
 @Module({
-  imports: [],
+  imports: [WorkoutsModule],
 })
 export class AppModule {}

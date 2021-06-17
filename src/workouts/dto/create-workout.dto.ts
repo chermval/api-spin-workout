d72b172e7638c575ea6b7f7d6cdd1c21e2a61040
Claudia <claudia@Claudias-MacBook-Pro.local>
2021-06-17 16:32:31 -0400

    add validation dto for save workout logic

--- src/workouts/dto/create-workout.dto.ts
+++ src/workouts/dto/create-workout.dto.ts
@@ -1,7 +1,18 @@
+import { IsNotEmpty } from 'class-validator'
+
 export class CreateWorkoutDto {
+    @IsNotEmpty()
     distance: String;
+
+    @IsNotEmpty()
     time: String;
+
+    @IsNotEmpty()
     velocity: String;
+
+    @IsNotEmpty()
     pulse: String; 
+    
+    @IsNotEmpty()
     date: String;
 }

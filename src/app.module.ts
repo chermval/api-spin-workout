6f979d256857c2d0a81b903a3291fadfb30f4673
Claudia <claudia@Claudias-MacBook-Pro.local>
2021-06-22 16:21:04 -0400

    add db port to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { WorkoutsModule } from './workouts/workouts.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5432,
+      port: parseInt(process.env.DB_PORT || '') ,
       database: process.env.DB_DATABASE,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
